2810ac62f88fca3f1aa538895957509de69d2bc4
Keith Gable <keith@art19.com>
2020-09-23 22:44:27 -0700

    Add missing provides

--- resources/default.rb
+++ resources/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+provides :zpool
 resource_name :zpool
 
 property :ashift, Integer, default: 0
